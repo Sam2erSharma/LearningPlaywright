8b646972df89b164348385f9013c2496cb7df971
Sam2erSharma <sameersharma2892@gmail.com>
2026-03-04 00:23:32 +0530

    Fix syntax error and implement verdict logic in 02_Test_Case_Result_Counter.js

--- PW_JS_Test_1/02_Test_Case_Result_Counter.js
+++ PW_JS_Test_1/02_Test_Case_Result_Counter.js
@@ -4,3 +4,27 @@ After a test suite runs, you receive an array of test results (strings: "pass", 
 input: testResults = ["pass", "pass", "fail", "pass", "skip", "pass", "fail", "pass"]
 output: Total Tests : 8 Passed: 5 Failed: 2 Skipped: 1 Pass Rate: 62.50% VERDICT: Minor failures. Review before release.
 */
+let testResults = ["pass", "pass", "fail", "pass", "skip", "pass", "fail", "pass"]
+let passed = 0, failed = 0, skipped = 0;
+
+for (let count = 0; count <= testResults.length - 1; count++) {
+    if (testResults[count] === "pass")
+        passed++;
+    else if (testResults[count] === "fail")
+        failed++;
+    else
+        skipped++;
+}
+console.log("Total Tests : " + testResults.length);
+console.log("Passed : " + passed);
+console.log("Failed : " + failed);
+console.log("Skipped : " + skipped);
+let passRate = (passed / testResults.length) * 100;
+console.log("Pass Rate : " + passRate.toFixed(2) + "%");
+if (passRate === 100) {
+    console.log("VERDICT: Ready for release");
+} else if (failed <= 2) {
+    console.log("VERDICT: Minor failures. Review before release.");
+} else {
+    console.log("VERDICT: Major failures. Block release");
+}
